074c2464cd8b4a826ff701bbefea35afac8d04b9
CPTblackadder <rob@robvandyk.com>
2025-12-02 22:03:39 +0000

    day 1 part 2 and day 2

--- src/day_01.rs
+++ src/day_01.rs
@@ -4,6 +4,68 @@ use crate::TaskCompleter;
 
 pub struct Task1;
 
+struct Dial {
+    position: i32,
+}
+
+impl Dial {
+    fn rotate_left(&mut self, amount: i32) -> i32 {
+        let full_rotations = amount / 100;
+        let remainder = amount % 100;
+        let start = self.position;
+        self.position = self.position - remainder;
+        let extra = if remainder > 0 {
+            if self.position == 0 {
+                1
+            } else if self.position < 0 {
+                self.position += 100;
+                if start != 0 { 1 } else { 0 }
+            } else {
+                0
+            }
+        } else {
+            0
+        };
+        // println!(
+        //     "-{amount} turn {} -> {}, adding {}",
+        //     start,
+        //     self.position,
+        //     full_rotations + extra
+        // );
+        assert!(self.position >= 0);
+        assert!(self.position < 100);
+        full_rotations + extra
+    }
+
+    fn rotate_right(&mut self, amount: i32) -> i32 {
+        let full_rotations = amount / 100;
+        let remainder = amount % 100;
+        let start = self.position;
+        self.position = self.position + remainder;
+        let extra = if remainder > 0 {
+            if self.position == 0 {
+                1
+            } else if self.position >= 100 {
+                self.position -= 100;
+                1
+            } else {
+                0
+            }
+        } else {
+            0
+        };
+        // println!(
+        //     "+{amount} turn {} -> {}, adding {}",
+        //     start,
+        //     self.position,
+        //     full_rotations + extra
+        // );
+        assert!(self.position >= 0);
+        assert!(self.position < 100);
+        full_rotations + extra
+    }
+}
+
 impl TaskCompleter for Task1 {
     fn do_task_1(&self) -> String {
         let lines = include_str!("../input/day_01/input").lines().map(|x| {
@@ -32,10 +94,14 @@ impl TaskCompleter for Task1 {
             let y = x[1..].parse::<i32>().unwrap();
             (d, y)
         });
-        let mut dial = 50;
+        let mut dial = Dial { position: 50 };
         let mut count = 0;
         for (direction, amount) in lines {
-            
+            count += if direction == 'L' {
+                dial.rotate_left(amount)
+            } else {
+                dial.rotate_right(amount)
+            }
         }
         count.to_string()
     }
@@ -45,6 +111,6 @@ impl TaskCompleter for Task1 {
     }
 
     fn task_2_result(&self) -> Option<String> {
-        None
+        Some("6684".to_string())
     }
 }
--- src/day_02.rs
+++ src/day_02.rs
@@ -1,21 +1,116 @@
+use std::collections::HashSet;
+
+use itertools::Itertools;
+
 use crate::TaskCompleter;
-    
+
 pub struct Task2;
+
+struct RangeCollection {
+    ranges: Vec<(i64, i64)>,
+}
+
+impl RangeCollection {
+    fn construct<T: Iterator<Item = (i64, i64)>>(ranges: T) -> Self {
+        let mut rc = Self { ranges: vec![] };
+        for (start, end) in ranges {
+            assert!(!rc.is_in(start));
+            assert!(!rc.is_in(end));
+            rc.ranges.push((start, end));
+        }
+        rc.ranges.sort();
+        rc
+    }
+
+    fn is_in(&self, value: i64) -> bool {
+        for (start, end) in &self.ranges {
+            if value < *start {
+                return false;
+            } else if value <= *end {
+                return true;
+            }
+        }
+        false
+    }
+
+    fn max(&self) -> i64 {
+        self.ranges.last().unwrap().1
+    }
+}
 
 impl TaskCompleter for Task2 {
     fn do_task_1(&self) -> String {
-        "".to_string()
+        let ranges =
+            RangeCollection::construct(include_str!("../input/day_02/input").split(",").map(|x| {
+                let mut s = x.split("-");
+                let first = s.next().unwrap().trim().parse::<i64>().unwrap();
+                let second = s.next().unwrap().trim().parse::<i64>().unwrap();
+                (first, second)
+            }));
+
+        let max_str = ranges.max().to_string();
+        let max_length = max_str.len();
+        let max = max_str
+            .chars()
+            .take(max_length / 2)
+            .collect::<String>()
+            .parse::<i64>()
+            .unwrap();
+
+        (0..max)
+            .map(|x| {
+                let v = format!("{}{}", x, x).parse::<i64>().unwrap();
+                if ranges.is_in(v) { v } else { 0 }
+            })
+            .sum::<i64>()
+            .to_string()
     }
 
     fn do_task_2(&self) -> String {
-        "".to_string()
+        let ranges =
+            RangeCollection::construct(include_str!("../input/day_02/input").split(",").map(|x| {
+                let mut s = x.split("-");
+                let first = s.next().unwrap().trim().parse::<i64>().unwrap();
+                let second = s.next().unwrap().trim().parse::<i64>().unwrap();
+                (first, second)
+            }));
+
+        let mut v = 1;
+        let mut coll = HashSet::new();
+        let mut count = 0;
+        loop {
+            let mut x = format!("{}{}", v, v).parse::<i64>().unwrap();
+            if x > ranges.max() {
+                break;
+            }
+            if !coll.contains(&x) {
+                if ranges.is_in(x) {
+                    count += x;
+                    coll.insert(x);
+                }
+            }
+
+            while x <= ranges.max() {
+                x = format!("{}{}", x, v).parse::<i64>().unwrap();
+                if !coll.contains(&x) {
+                    if ranges.is_in(x) {
+                        count += x;
+                        coll.insert(x);
+                    }
+                }
+            }
+
+            v += 1;
+        }
+
+        count.to_string()
     }
 
     fn task_1_result(&self) -> Option<String> {
-        None
+        Some("21898734247".to_string())
     }
 
     fn task_2_result(&self) -> Option<String> {
-        None
+        Some("28915664389".to_string())
     }
 }
