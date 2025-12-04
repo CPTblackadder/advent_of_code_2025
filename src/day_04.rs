658796d41f335d997cda36b4f7753045d9e373f4
CPTblackadder <rob@robvandyk.com>
2025-12-04 12:55:35 +0000

    day 4

--- src/day_04.rs
+++ src/day_04.rs
@@ -1,21 +1,63 @@
-use crate::TaskCompleter;
-    
+use itertools::Itertools;
+
+use crate::{TaskCompleter, grid::Grid};
+
 pub struct Task4;
 
 impl TaskCompleter for Task4 {
     fn do_task_1(&self) -> String {
-        "".to_string()
+        let grid = Grid::from_string(include_str!("../input/day_04/input"), false);
+        grid.iter()
+            .filter(|x| {
+                *x.1 == '@'
+                    && x.0
+                        .get_bounded_neighbours(&grid, true)
+                        .iter()
+                        .filter(|x| grid[**x] == '@')
+                        .count()
+                        < 4
+            })
+            .count()
+            .to_string()
     }
 
     fn do_task_2(&self) -> String {
-        "".to_string()
+        let mut grid = Grid::from_string(include_str!("../input/day_04/input"), false);
+        let mut removed = 0;
+        loop {
+            let to_remove = grid
+                .iter()
+                .filter_map(|x| {
+                    if *x.1 == '@'
+                        && x.0
+                            .get_bounded_neighbours(&grid, true)
+                            .iter()
+                            .filter(|x| grid[**x] == '@')
+                            .count()
+                            < 4
+                    {
+                        Some(x.0)
+                    } else {
+                        None
+                    }
+                })
+                .collect_vec();
+            if to_remove.is_empty() {
+                break;
+            }
+            removed += to_remove.len();
+            for coord in to_remove {
+                grid[coord] = '.'
+            }
+        }
+        removed.to_string()
     }
 
     fn task_1_result(&self) -> Option<String> {
-        None
+        Some(1351.to_string())
     }
 
     fn task_2_result(&self) -> Option<String> {
-        None
+        Some(8345.to_string())
     }
 }
